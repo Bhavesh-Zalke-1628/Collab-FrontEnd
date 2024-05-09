14b19cbd82050865770e51f638ad0c1e5a572bcd
Bhavesh Zalke <zalkebhavesh@gmail.com>
2024-05-09 09:14:22 +0530

    Add the card

---
+++ src/components/Constant/PlanCard.jsx
@@ -0,0 +1,24 @@
+import { FormControlLabel, Radio } from '@mui/material'
+import React from 'react'
+
+function PlanCard({ plan }) {
+    console.log(plan)
+    return (
+        <a
+            href='#'
+            class='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 relative min-w-72'
+        >
+            <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                {plan.planName}
+            </h5>
+            <div className='text-4xl font-semibold'> {plan.price}</div>
+            <FormControlLabel
+                value='under 18 with coaching'
+                control={<Radio />}
+                label={plan.planName}
+            />
+        </a>
+    )
+}
+
+export default PlanCard
--- src/components/stepper/ScheduleStep3.jsx
+++ src/components/stepper/ScheduleStep3.jsx
@@ -1,133 +1,58 @@
-import { Button } from '@mui/material'
-import React, { useEffect } from 'react'
-import { getRazorPayId, purchaseCourseBundle } from '../../redux/slices/paymentSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { useNavigate } from 'react-router-dom';
-import toast from 'react-hot-toast';
+import {
+    FormControl,
+    FormControlLabel,
+    FormLabel,
+    RadioGroup
+} from '@mui/material'
+import React, { useState } from 'react'
+import Radio from '@mui/material/Radio'
+import axios from 'axios'
+import { useEffect } from 'react'
+import PlanCard from '../Constant/PlanCard'
 
 const ScheduleStep3 = () => {
+    const [male, setmale] = useState(true)
+    const [morning, setmorning] = useState()
+    const [data, setData] = useState()
+    async function getplan() {
+        const response = await axios.get('http://localhost:5000/api/plan/getAllPlan')
 
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const razorpayKey = useSelector((state) => state?.razorpay?.key);
-    const subscription_id = useSelector((state) => state?.razorpay?.subscription_id);
-    const data = JSON.parse(localStorage.getItem('userData'))
-
-    const paymentDetails = {
-        razorpay_payment_id: "",
-        razorpay_subscription_id: "",
-        razorpay_signature: ""
+        console.log(response.data)
+        setData(response.data)
     }
-
-    async function handleSubscription(e) {
-        e.preventDefault();
-        if (!razorpayKey || !subscription_id) {
-            toast.error("Something went wrong");
-            return;
-        }
-        const options = {
-            key: razorpayKey,
-            subscription_id: subscription_id,
-            name: "e-Shiksha Pvt. Ltd.",
-            description: "Subscription",
-            theme: {
-                color: '#F37254'
-            },
-
-            handler: async function (response) {
-                paymentDetails.razorpay_payment_id = response.razorpay_payment_id;
-                paymentDetails.razorpay_signature = response.razorpay_signature;
-                paymentDetails.razorpay_subscription_id = response.razorpay_subscription_id;
-                toast.success("Payment successfull");
-                const res = await dispatch(verifyUserPayment([paymentDetails, data._id]));
-                console.log('navigate res', res)
-                toast.success(res?.payload?.msg);
-                res?.payload?.success ? navigate("/checkout/success") : navigate("/checkout/fail");
-            }
-        }
-        const paymentObject = new window.Razorpay(options);
-        paymentObject.open();
-    }
-
-    async function load() {
-        await dispatch(getRazorPayId(data._id));
-        await dispatch(purchaseCourseBundle(data._id));
-    }
+    console.log(data)
 
     useEffect(() => {
-        load();
+        getplan()
     }, []);
-
     return (
         <div>
             <div>
-                <div className='flex gap-2'>
-                    <a
-                        href='#'
-                        class='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 relative'
-                    >
-                        <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                            plan under 18
-                        </h5>
-                        <p class='font-normal text-gray-700 dark:text-gray-400'>
-                            Here are the biggest enterprise technology acquisitions of 2021 so
-                            far, in reverse chronological order.
+                {
+                    // data.map(e => console.log(e))
+                }
+                <FormControl>
+                    <FormLabel id='demo-radio-buttons-group-label'>
+                        {' '}
+                        <p className='text-4xl text-black  m-5 font-semibold p-2'>
+                            ➡ please select any one batch
                         </p>
-                        <input
-                            className=' absolute   right-0 w-20  border-2 '
-                            type='radio'
-                        />
-                    </a>
-                    <a
-                        href='#'
-                        class='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 relative'
+                    </FormLabel>
+                    <RadioGroup
+                        aria-labelledby='demo-radio-buttons-group-label'
+                        defaultValue='female'
+                        name='radio-buttons-group'
                     >
-                        <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                            plan 18 + coaching
-                        </h5>
-                        <p class='font-normal text-gray-700 dark:text-gray-400'>
-                            Here are the biggest enterprise technology acquisitions of 2021 so
-                            far, in reverse chronological order.
-                        </p>
-                        <input
-                            className=' absolute   right-0 w-20  border-2 '
-                            type='radio'
-                        />
-                    </a>
-                    <a
-                        href='#'
-                        class='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 relative'
-                    >
-                        <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                            plan above 18
-                        </h5>
-                        <p class='font-normal text-gray-700 dark:text-gray-400'>
-                            Here are the biggest enterprise technology acquisitions of 2021 so
-                            far, in reverse chronological order.
-                        </p>
-                        <input
-                            className=' absolute  right-0 w-20  border-2 '
-                            type='radio'
-                        />
-                    </a>
-
-                    <a
-                        href='#'
-                        class='block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 relative'
-                    >
-                        <h5 class='mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white'>
-                            plan above 18 + coaching
-                        </h5>
-                        <p class='font-normal text-gray-700 dark:text-gray-400'>
-                            Here are the biggest enterprise technology acquisitions of 2021 so
-                            far, in reverse chronological order.
-                        </p>
-                        <input
-                            className=' absolute   right-0 w-20  border-2 '
-                            type='radio'
-                        />
-                    </a>
-                </div>
+                        {/* /> */}
+                        <div className='flex gap-2'>
+                            {data &&
+                                data.map((e) => {
+                                    return <PlanCard plan={e} />
+                                })
+                            }
+                        </div>
+                    </RadioGroup>
+                </FormControl>
 
                 <div className='flex mt-10'>
                     <form class='max-w-sm mx-auto'>
@@ -142,39 +67,71 @@ const ScheduleStep3 = () => {
                             class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
                         >
                             <option selected>Choose a gender</option>
-                            <option value='US'>male</option>
-                            <option value='CA'>felmale</option>
-                            <option value='FR'>others</option>
-                        </select>
-                    </form>
-
-                    <form class='max-w-sm mx-auto'>
-                        <label
-                            for='countries'
-                            class='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-                        >
-                            Select an option
-                        </label>
-                        <select
-                            id='countries'
-                            class='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-                        >
-                            <option selected>Choose a time</option>
-                            <option value='US'>morming</option>
-                            <option value='CA'>after noon</option>
-                            <option value='FR'>evening</option>
+                            <option value='male'>male</option>
+                            <option value='female'>female</option>
                         </select>
                     </form>
                 </div>
+                <div className='flex items-center justify-center'>
+                    {male && (
+                        <FormControl>
+                            <FormLabel id='demo-radio-buttons-group-label'>
+                                morning batch
+                            </FormLabel>
+                            <RadioGroup
+                                aria-labelledby='demo-radio-buttons-group-label'
+                                defaultValue=' 7:00 to 8:00'
+                                name='radio-buttons-group'
+                            >
+                                <FormControlLabel
+                                    value=' 7:00 to 8:00'
+                                    control={<Radio />}
+                                    label='7:00 to 8:00'
+                                />
+                                <FormControlLabel
+                                    value='9:00 to 11:00'
+                                    control={<Radio />}
+                                    label='9:00 to 11:00'
+                                />
+                                <FormControlLabel
+                                    value='11:00 to 12:000'
+                                    control={<Radio />}
+                                    label='11:00 to  12:00'
+                                />
+                            </RadioGroup>
+                        </FormControl>
+                    )}
+                    {/* 
+          {!male && (
+            <FormControl>
+              <FormLabel id='demo-radio-buttons-group-label'>
+                morning batch
+              </FormLabel>
+              <RadioGroup
+                aria-labelledby='demo-radio-buttons-group-label'
+                defaultValue=' 7:00 to 8:00'
+                name='radio-buttons-group'
+              >
+                <FormControlLabel
+                  value=' 7:00 to 8:00'
+                  control={<Radio />}
+                  label='7:00 to 8:00'
+                />
+                <FormControlLabel
+                  value='9:00 to 11:00'
+                  control={<Radio />}
+                  label='9:00 to 11:00'
+                />
+                <FormControlLabel
+                  value='11:00 to 12:000'
+                  control={<Radio />}
+                  label='11:00 to  12:00'
+                />
+              </RadioGroup>
+            </FormControl>
+          )} */}
+                </div>
             </div>
-            <Button
-                variant='contained'
-                color='secondary'
-                className=' mr-5'
-                onClick={handleSubscription}
-            >
-                save and Continue
-            </Button>
         </div>
     )
 }
